d944a342a141748484542c875f05603693efe569
onlystatic <onlystatic@msn.com>
2020-11-04 19:57:03 +0800

    Config client.

--- cloud-config-client3355/src/main/java/com/noonhope/springcloud/ConfigClientMain3355.java
+++ cloud-config-client3355/src/main/java/com/noonhope/springcloud/ConfigClientMain3355.java
@@ -2,19 +2,17 @@ package com.noonhope.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author v_qianglong
- * @date 2020/11/4 15:04
+ * @date 2020/11/4 19:36
  */
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigCenterMain3344 {
+public class ConfigClientMain3355 {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCenterMain3344.class, args);
+        SpringApplication.run(ConfigClientMain3355.class, args);
     }
 }
