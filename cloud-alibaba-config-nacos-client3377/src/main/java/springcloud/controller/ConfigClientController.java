b648b227ce9074541590a3aebc21cda23971f614
onlystatic <onlystatic@msn.com>
2020-11-08 19:46:01 +0800

    Nacos config.

---
+++ cloud-alibaba-config-nacos-client3377/src/main/java/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author onlystatic
+ * @date 2020-11-07 06:32 PM
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
