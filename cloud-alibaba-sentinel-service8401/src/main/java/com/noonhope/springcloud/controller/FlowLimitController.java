d1118d4e5d1bf91fdff86fd00dbe92de9a8dacb2
onlystatic <onlystatic@msn.com>
2020-11-09 20:07:42 +0800

    Sentinel.

--- cloud-alibaba-sentinel-service8401/src/main/java/com/noonhope/springcloud/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/noonhope/springcloud/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.noonhope.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,5 +21,17 @@ public class FlowLimitController {
     @GetMapping("/test_b")
     public String testB() {
         return "-----------testB";
+    }
+
+    @GetMapping("/test_hot_key")
+    @SentinelResource(value = "test_test_hot_key", blockHandler = "testHotKeyHandler")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+            @RequestParam(value = "p1", required = false) String p2) {
+        //int age = 10 / 0;
+        return "testHotKey...";
+    }
+
+    public String testHotKeyHandler(String p1, String p2, BlockException blockException) {
+        return "testHotKeyHandler...";
     }
 }
